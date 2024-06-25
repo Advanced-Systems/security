723a8b94a3173d9a2df6147213450c28bf35204c
StefanGreve <greve.stefan@outlook.jp>
2024-06-25 19:24:56 +0200

    Init commit

---
+++ AdvancedSystems.Security.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AdvancedSystems.Security.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
