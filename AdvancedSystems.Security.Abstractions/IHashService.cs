102c03222ceca3842130e36d07a044b8ab64d5cf
StefanGreve <greve.stefan@outlook.jp>
2024-06-26 21:26:31 +0200

    Improve description of the return type in IHashService

--- AdvancedSystems.Security.Abstractions/IHashService.cs
+++ AdvancedSystems.Security.Abstractions/IHashService.cs
@@ -11,7 +11,7 @@ namespace AdvancedSystems.Security.Abstractions
         ///     Computes the MD5 hash value for the specified byte array.
         /// </summary>
         /// <param name="buffer">The input to compute the hash code for.</param>
-        /// <returns>The computed hash code.</returns>
+        /// <returns>The hexadecimal <seealso cref="string"/> representation of the computed hash code.</returns>
         /// <seealso href="https://datatracker.ietf.org/doc/rfc1321/"/>
         [Obsolete("MD5 is not a cryptographically secure hash algorithm.")]
         string GetMD5Hash(byte[] buffer);
@@ -20,7 +20,7 @@ namespace AdvancedSystems.Security.Abstractions
         ///     Computes the SHA1 hash value for the specified byte array.
         /// </summary>
         /// <param name="buffer">The input to compute the hash code for.</param>
-        /// <returns>The computed hash code.</returns>
+        /// <returns>The hexadecimal <seealso cref="string"/> representation of the computed hash code.</returns>
         /// <seealso href="https://datatracker.ietf.org/doc/rfc3174/"/>
         [Obsolete("SHA1 is not a cryptographically secure hash algorithm.")]
         string GetSHA1Hash(byte[] buffer);
@@ -29,7 +29,7 @@ namespace AdvancedSystems.Security.Abstractions
         ///     Computes the SHA256 hash value for the specified byte array.
         /// </summary>
         /// <param name="buffer">The input to compute the hash code for.</param>
-        /// <returns>The computed hash code.</returns>
+        /// <returns>The hexadecimal <seealso cref="string"/> representation of the computed hash code.</returns>
         /// <seealso href="https://datatracker.ietf.org/doc/rfc4634/"/>
         string GetSHA256Hash(byte[] buffer);
 
@@ -37,7 +37,7 @@ namespace AdvancedSystems.Security.Abstractions
         ///     Computes the SHA384 hash value for the specified byte array.
         /// </summary>
         /// <param name="buffer">The input to compute the hash code for.</param>
-        /// <returns>The computed hash code.</returns>
+        /// <returns>The hexadecimal <seealso cref="string"/> representation of the computed hash code.</returns>
         /// <seealso href="https://datatracker.ietf.org/doc/rfc6234/"/>
         string GetSHA384Hash(byte[] buffer);
 
@@ -45,7 +45,7 @@ namespace AdvancedSystems.Security.Abstractions
         ///     Computes the SHA512 hash value for the specified byte array.
         /// </summary>
         /// <param name="buffer">The input to compute the hash code for.</param>
-        /// <returns>The computed hash code.</returns>
+        /// <returns>The hexadecimal <seealso cref="string"/> representation of the computed hash code.</returns>
         /// <seealso href="https://datatracker.ietf.org/doc/rfc4634/"/>
         string GetSHA512Hash(byte[] buffer);
     }
