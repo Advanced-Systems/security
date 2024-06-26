8842533abfbc1d55a6d882e5b3f0698400c350f9
StefanGreve <greve.stefan@outlook.jp>
2024-06-26 21:46:10 +0200

    Implement GetBytes extension method for the SecureString class

--- AdvancedSystems.Security/Common/Bytes.cs
+++ AdvancedSystems.Security/Common/Bytes.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 
 namespace AdvancedSystems.Security.Common
@@ -36,6 +38,39 @@ namespace AdvancedSystems.Security.Common
             ulong ulongValue => BitConverter.GetBytes(ulongValue),
             _ => throw new ArgumentException("Failed to convert T to an array of bytes.", nameof(value))
         };
+
+        /// <summary>
+        ///     Gets the bytes of a <see cref="SecureString"/> object.
+        /// </summary>
+        /// <param name="secureString">Represents a text that should be kept confidential, such as a password.</param>
+        /// <param name="encoding">The character encoding used to convert all characters in the underlying <seealso cref="string"/> into a sequence of bytes.</param>
+        /// <returns>Returns the bytes of the current instance.</returns>
+        /// <remarks>
+        ///     This method exposes the internal state of <see cref="SecureString"/> through marshaling.
+        ///     It is no longer recommended to use the <see cref="SecureString"/> for new development on .NET Core projects.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Raised if <paramref name="secureString"/> is <seealso cref="null"/>.</exception>
+        /// <seealso href="https://github.com/dotnet/platform-compat/blob/master/docs/DE0001.md"/>
+        [Obsolete]
+        public static byte[] GetBytes(this SecureString secureString, Encoding? encoding = null)
+        {
+            ArgumentNullException.ThrowIfNull(secureString, nameof(secureString));
+            encoding ??= Encoding.UTF8;
+            IntPtr strPtr = IntPtr.Zero;
+
+            try
+            {
+                strPtr = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return encoding.GetBytes(Marshal.PtrToStringUni(strPtr)!);
+            }
+            finally
+            {
+                if (strPtr != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeGlobalAllocUnicode(strPtr);
+                }
+            }
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool AreEqual(byte[] lhs, byte[] rhs)
