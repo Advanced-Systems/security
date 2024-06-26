187aa037a9229d5fa46416d4eab58a7537ce25ef
StefanGreve <greve.stefan@outlook.jp>
2024-06-26 21:07:06 +0200

    Implement HashService

---
+++ AdvancedSystems.Security.Abstractions/IHashService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AdvancedSystems.Security.Abstractions
+{
+    /// <summary>
+    ///     Defines a methods for computing hash codes.
+    /// </summary>
+    public interface IHashService
+    {
+        /// <summary>
+        ///     Computes the MD5 hash value for the specified byte array.
+        /// </summary>
+        /// <param name="buffer">The input to compute the hash code for.</param>
+        /// <returns>The computed hash code.</returns>
+        /// <seealso href="https://datatracker.ietf.org/doc/rfc1321/"/>
+        [Obsolete("MD5 is not a cryptographically secure hash algorithm.")]
+        string GetMD5Hash(byte[] buffer);
+
+        /// <summary>
+        ///     Computes the SHA1 hash value for the specified byte array.
+        /// </summary>
+        /// <param name="buffer">The input to compute the hash code for.</param>
+        /// <returns>The computed hash code.</returns>
+        /// <seealso href="https://datatracker.ietf.org/doc/rfc3174/"/>
+        [Obsolete("SHA1 is not a cryptographically secure hash algorithm.")]
+        string GetSHA1Hash(byte[] buffer);
+
+        /// <summary>
+        ///     Computes the SHA256 hash value for the specified byte array.
+        /// </summary>
+        /// <param name="buffer">The input to compute the hash code for.</param>
+        /// <returns>The computed hash code.</returns>
+        /// <seealso href="https://datatracker.ietf.org/doc/rfc4634/"/>
+        string GetSHA256Hash(byte[] buffer);
+
+        /// <summary>
+        ///     Computes the SHA384 hash value for the specified byte array.
+        /// </summary>
+        /// <param name="buffer">The input to compute the hash code for.</param>
+        /// <returns>The computed hash code.</returns>
+        /// <seealso href="https://datatracker.ietf.org/doc/rfc6234/"/>
+        string GetSHA384Hash(byte[] buffer);
+
+        /// <summary>
+        ///     Computes the SHA512 hash value for the specified byte array.
+        /// </summary>
+        /// <param name="buffer">The input to compute the hash code for.</param>
+        /// <returns>The computed hash code.</returns>
+        /// <seealso href="https://datatracker.ietf.org/doc/rfc4634/"/>
+        string GetSHA512Hash(byte[] buffer);
+    }
+}
---
+++ AdvancedSystems.Security/Services/HashService.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography;
+
+using AdvancedSystems.Security.Abstractions;
+using AdvancedSystems.Security.Common;
+using AdvancedSystems.Security.Cryptography;
+
+using Microsoft.Extensions.Logging;
+
+namespace AdvancedSystems.Security.Services;
+
+public sealed class HashService : IHashService
+{
+    private readonly ILogger<HashService> _logger;
+
+    public HashService(ILogger<HashService> logger)
+    {
+        this._logger = logger;
+    }
+
+    public string GetMD5Hash(byte[] buffer)
+    {
+        this._logger.LogWarning("Computing hash with a cryptographically insecure hash algorithm (MD5).");
+
+        byte[] md5 = Hash.Compute(buffer, HashAlgorithmName.MD5);
+        return md5.ToString(Format.Hex);
+    }
+
+    public string GetSHA1Hash(byte[] buffer)
+    {
+        this._logger.LogWarning("Computing hash with a cryptographically insecure hash algorithm (SHA1.)");
+
+        byte[] sha1 = Hash.Compute(buffer, HashAlgorithmName.SHA1);
+        return sha1.ToString(Format.Hex);
+    }
+
+    public string GetSHA256Hash(byte[] buffer)
+    {
+        byte[] sha256 = Hash.Compute(buffer, HashAlgorithmName.SHA256);
+        return sha256.ToString(Format.Hex);
+    }
+
+    public string GetSHA384Hash(byte[] buffer)
+    {
+        byte[] sha384 = Hash.Compute(buffer, HashAlgorithmName.SHA384);
+        return sha384.ToString(Format.Hex);
+    }
+
+    public string GetSHA512Hash(byte[] buffer)
+    {
+        byte[] sha512 = Hash.Compute(buffer, HashAlgorithmName.SHA512);
+        return sha512.ToString(Format.Hex);
+    }
+}
