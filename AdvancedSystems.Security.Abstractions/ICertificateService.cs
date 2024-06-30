6bbe7169ffcf462b320ff85cfb9bd85e1ad90e48
StefanGreve <greve.stefan@outlook.jp>
2024-06-30 12:47:03 +0200

    Update interface definitions in abstractions project

--- AdvancedSystems.Security.Abstractions/ICertificateService.cs
+++ AdvancedSystems.Security.Abstractions/ICertificateService.cs
@@ -1,7 +1,15 @@
-﻿namespace AdvancedSystems.Security.Abstractions
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace AdvancedSystems.Security.Abstractions
 {
     public interface ICertificateService
     {
+        #region Methods
 
+        X509Certificate2? GetStoreCertificate(StoreName storeName, StoreLocation storeLocation, string thumbprint);
+
+        X509Certificate2? GetConfiguredCertificate();
+
+        #endregion
     }
 }
--- AdvancedSystems.Security.Abstractions/IRSACryptoService.cs
+++ AdvancedSystems.Security.Abstractions/IRSACryptoService.cs
@@ -1,36 +1,37 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace AdvancedSystems.Security.Abstractions
 {
-    public interface IRSACryptoProvider
+    public interface IRSACryptoService : IDisposable
     {
         #region Properties
 
         X509Certificate2 Certificate { get; }
 
-        HashAlgorithmName HashAlgorithmName { get; set; }
+        HashAlgorithmName HashAlgorithmName { get; }
 
-        RSAEncryptionPadding EncryptionPadding { get; set; }
+        RSAEncryptionPadding EncryptionPadding { get; }
 
-        RSASignaturePadding SignaturePadding { get; set; }
+        RSASignaturePadding SignaturePadding { get; }
 
-        Encoding Encoding { get; set; }
+        Encoding Encoding { get; }
 
         #endregion
 
         #region Methods
 
-        bool IsValidMessage(string message, RSAEncryptionPadding? padding = null, Encoding? encoding = null);
+        bool IsValidMessage(string message, RSAEncryptionPadding? padding, Encoding? encoding = null);
 
         string Encrypt(string message, Encoding? encoding = null);
 
         string Decrypt(string cipher, Encoding? encoding = null);
 
-        string Sign(string data, Encoding? encoding = null);
+        string SignData(string data, Encoding? encoding = null);
 
-        bool Verify(string data, string signature, Encoding? encoding = null);
+        bool VerifyData(string data, string signature, Encoding? encoding = null);
 
         #endregion
     }
