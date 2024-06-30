4e253cd1b93ede4e50989403a579b677681f06be
StefanGreve <greve.stefan@outlook.jp>
2024-06-30 12:48:39 +0200

    Implement CertificateService and ExceptionFilter
    * add CertificateOptions to service via DI

---
+++ AdvancedSystems.Security/Options/CertificateOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AdvancedSystems.Security.Options;
+
+public record CertificateOptions
+{
+    [Key]
+    public required string Thumbprint { get; init; }
+
+    [EnumDataType(typeof(StoreName))]
+    public required StoreName StoreName { get; init; }
+
+    [EnumDataType(typeof(StoreLocation))]
+    public required StoreLocation StoreLocation { get; init; }
+}
--- AdvancedSystems.Security/Services/CertificateService.cs
+++ AdvancedSystems.Security/Services/CertificateService.cs
@@ -1,15 +1,48 @@
-﻿using AdvancedSystems.Security.Abstractions;
+﻿using System.Security.Cryptography.X509Certificates;
+
+using AdvancedSystems.Security.Abstractions;
+using AdvancedSystems.Security.Cryptography;
+using AdvancedSystems.Security.Exceptions;
+using AdvancedSystems.Security.Options;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using static AdvancedSystems.Security.Internal.ExceptionFilter;
 
 namespace AdvancedSystems.Security.Services;
 
 public sealed class CertificateService : ICertificateService
 {
     private readonly ILogger<CertificateService> _logger;
+    private readonly IOptions<CertificateOptions> _options;
 
-    public CertificateService(ILogger<CertificateService> logger)
+    public CertificateService(ILogger<CertificateService> logger, IOptions<CertificateOptions> options)
     {
         this._logger = logger;
+        this._options = options;
     }
+
+    #region Public Methods
+
+    public X509Certificate2? GetStoreCertificate(StoreName storeName, StoreLocation storeLocation, string thumbprint)
+    {
+        try
+        {
+            using var _ = this._logger.BeginScope("Searching for {thumbprint} in {storeName} at {storeLocation}", thumbprint, storeName, storeLocation);
+            return Certificate.GetStoreCertificate(storeName, storeLocation, thumbprint);
+        }
+        catch (CertificateNotFoundException exception) when (True(() => this._logger.LogError(exception, "Service failed to retrieve certificate.")))
+        {
+            return null;
+        }
+    }
+
+    public X509Certificate2? GetConfiguredCertificate()
+    {
+        var config = this._options.Value;
+        return this.GetStoreCertificate(config.StoreName, config.StoreLocation, config.Thumbprint);
+    }
+
+    #endregion
 }
