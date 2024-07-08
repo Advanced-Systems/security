2f5d692f3c159ac6159704876c94b18b53f313a7
StefanGreve <greve.stefan@outlook.jp>
2024-07-08 12:48:55 +0200

    Switch from NUnit to XUnit

--- AdvancedSystems.Security.Tests/Services/HashServiceTests.cs
+++ AdvancedSystems.Security.Tests/Services/HashServiceTests.cs
@@ -3,21 +3,20 @@
 using AdvancedSystems.Security.Abstractions;
 using AdvancedSystems.Security.Tests.Fixtures;
 
+using Xunit;
+
 namespace AdvancedSystems.Security.Tests.Services;
 
-[TestFixture]
-public class HashServiceTests
+public class HashServiceTests : IClassFixture<HashServiceFixture>
 {
-    private IHashService _sut;
+    private readonly IHashService _sut;
 
-    [OneTimeSetUp]
-    public void Setup()
+    public HashServiceTests(HashServiceFixture fixture)
     {
-        var fixture = new HashServiceFixture();
         this._sut = fixture.HashService;
     }
 
-    [Test]
+    [Fact]
     public void TestMD5Hash()
     {
         // Arrange
@@ -30,10 +29,10 @@ public class HashServiceTests
 #pragma warning restore CS0618 // Type or member is obsolete
 
         // Assert
-        Assert.That(md5, Is.EqualTo("9e107d9d372bb6826bd81d3542a419d6"));
+        Assert.Equal("9e107d9d372bb6826bd81d3542a419d6", md5);
     }
 
-    [Test]
+    [Fact]
     public void TestSHA1Hash()
     {
         // Arrange
@@ -46,10 +45,10 @@ public class HashServiceTests
 #pragma warning restore CS0618 // Type or member is obsolete
 
         // Assert
-        Assert.That(sha1, Is.EqualTo("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"));
+        Assert.Equal("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", sha1);
     }
 
-    [Test]
+    [Fact]
     public void TestSHA256Hash()
     {
         // Arrange
@@ -60,10 +59,10 @@ public class HashServiceTests
         string sha256 = this._sut.GetSHA256Hash(buffer);
 
         // Assert
-        Assert.That(sha256, Is.EqualTo("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592"));
+        Assert.Equal("d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592", sha256);
     }
 
-    [Test]
+    [Fact]
     public void TestSHA384Hash()
     {
         // Arrange
@@ -74,10 +73,10 @@ public class HashServiceTests
         string sha384 = this._sut.GetSHA384Hash(buffer);
 
         // Assert
-        Assert.That(sha384, Is.EqualTo("ca737f1014a48f4c0b6dd43cb177b0afd9e5169367544c494011e3317dbf9a509cb1e5dc1e85a941bbee3d7f2afbc9b1"));
+        Assert.Equal("ca737f1014a48f4c0b6dd43cb177b0afd9e5169367544c494011e3317dbf9a509cb1e5dc1e85a941bbee3d7f2afbc9b1", sha384);
     }
 
-    [Test]
+    [Fact]
     public void TestSHA512Hash()
     {
         // Arrange
@@ -88,6 +87,6 @@ public class HashServiceTests
         string sha512 = this._sut.GetSHA512Hash(buffer);
 
         // Assert
-        Assert.That(sha512, Is.EqualTo("07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6"));
+        Assert.Equal("07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6", sha512);
     }
 }
