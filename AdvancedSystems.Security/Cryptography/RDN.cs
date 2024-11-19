1d33eb81d800bc15eade63590689506be127fdd3
StefanGreve <greve.stefan@outlook.jp>
2024-11-19 23:37:38 +0100

    Implement ParseDistinguishedName

---
+++ AdvancedSystems.Security/Cryptography/RDN.cs
@@ -0,0 +1,55 @@
+﻿namespace AdvancedSystems.Security.Cryptography;
+
+/// <summary>
+///     The Relative Distinguished Names (RDNs) are attribute-value pairs that
+///     uniquely identify an entity.
+/// </summary>
+/// <remarks>
+///     This class defines the attributes that annotate one or more values.
+/// </remarks>
+/// <seealso cref="DistinguishedName"/>
+/// <seealso href="https://datatracker.ietf.org/doc/html/rfc4514"/>
+public static class RDN
+{
+    /// <summary>
+    ///     Country (<c>C</c>)
+    /// </summary>
+    /// <remarks>
+    ///     This field could also be used to store the region.
+    /// </remarks>
+    public const string C = "C";
+
+    /// <summary>
+    ///     Common Name (<c>CN</c>)
+    /// </summary>
+    /// <remarks>
+    ///     This field denotes the certificate owner's common name.
+    /// </remarks>
+    public const string CN = "CN";
+
+    /// <summary>
+    ///     Locality (<c>L</c>)
+    /// </summary>
+    /// <remarks>
+    ///     This field could also be used to store the city.
+    /// </remarks>
+    public const string L = "L";
+
+    /// <summary>
+    ///     Organization (<c>O</c>)
+    /// </summary>
+    public const string O = "O";
+
+    /// <summary>
+    ///     Organizational Unit (<c>OU</c>)
+    /// </summary>
+    public const string OU = "OU";
+
+    /// <summary>
+    ///     State (<c>S</c>)
+    /// </summary>
+    /// <remarks>
+    ///     This field could also be used to store the province.
+    /// </remarks>
+    public const string S = "S";
+}
--- AdvancedSystems.Security/Extensions/CertificateExtensions.cs
+++ AdvancedSystems.Security/Extensions/CertificateExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 using AdvancedSystems.Security.Abstractions;
 using AdvancedSystems.Security.Abstractions.Exceptions;
+using AdvancedSystems.Security.Cryptography;
 
 namespace AdvancedSystems.Security.Extensions;
 
@@ -10,7 +13,7 @@ namespace AdvancedSystems.Security.Extensions;
 ///     Defines functions for interacting with X.509 certificates.
 /// </summary>
 /// <seealso href="https://datatracker.ietf.org/doc/rfc5280/"/>
-public static class CertificateStore
+public static partial class CertificateExtensions
 {
     /// <summary>
     ///     Retrieves an X.509 certificate from the specified store using the provided thumbprint.
@@ -41,5 +44,38 @@ public static class CertificateStore
 
         return certificate
             ?? throw new CertificateNotFoundException("No valid certificate matching the search criteria could be found in the store.");
+    }
+
+    public static DistinguishedName ParseDistinguishedName(string distinguishedName)
+    {
+        var rdns = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        var dn = new X500DistinguishedName(distinguishedName);
+
+        foreach (var rdn in dn.EnumerateRelativeDistinguishedNames())
+        {
+            string? attribute = rdn.GetSingleElementType().FriendlyName;
+            string value = rdn.GetSingleElementValue() ?? string.Empty;
+
+            if (string.IsNullOrEmpty(attribute)) continue;
+
+            rdns.Add(attribute, value);
+        }
+
+        rdns.TryGetValue(RDN.C, out string? country);
+        rdns.TryGetValue(RDN.CN, out string? commonName);
+        rdns.TryGetValue(RDN.L, out string? locality);
+        rdns.TryGetValue(RDN.O, out string? organization);
+        rdns.TryGetValue(RDN.OU, out string? organizationalUnit);
+        rdns.TryGetValue(RDN.S, out string? state);
+
+        return new DistinguishedName
+        {
+            CommonName = commonName,
+            OrganizationalUnit = organizationalUnit,
+            Organization = organization,
+            Locality = locality,
+            State = state,
+            Country = country,
+        };
     }
 }
