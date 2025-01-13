1158521e9938f79b55d85f679ab03d4865c05d53
StefanGreve <greve.stefan@outlook.jp>
2025-01-13 20:41:09 +0100

    Add more tests to CertificateServiceTests

--- AdvancedSystems.Security.Tests/Services/CertificateServiceTests.cs
+++ AdvancedSystems.Security.Tests/Services/CertificateServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 using AdvancedSystems.Security.Abstractions;
 using AdvancedSystems.Security.Tests.Fixtures;
@@ -24,8 +27,30 @@ public sealed class CertificateServiceTests : IClassFixture<CertificateFixture>
     #region Tests
 
     /// <summary>
+    ///     Tests that <seealso cref="ICertificateService.GetCertificate(string)"/>
+    ///     returns a collection of certificates from the configured certificate1 store.
+    /// </summary>
+    [Fact]
+    public void TestGetCertificate()
+    {
+        // Arrange
+        string storeService = this._sut.ConfiguredStoreService;
+
+        // Act
+        ICertificateService? certificateService = this._sut.Host?.Services.GetService<ICertificateService>();
+        int? certificateCount = certificateService?.GetCertificate(storeService).Count();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.NotNull(certificateCount);
+            Assert.InRange(certificateCount.Value, 1, int.MaxValue);
+        });
+    }
+
+    /// <summary>
     ///     Tests that <seealso cref="ICertificateService.GetCertificate(string, string, bool)"/>
-    ///     returns a certificate from the certificate store.
+    ///     returns a certificate1 from the certificate1 store.
     /// </summary>
     [Fact]
     public void TestGetCertificate_ByThumbprint()
@@ -44,6 +69,49 @@ public sealed class CertificateServiceTests : IClassFixture<CertificateFixture>
             Assert.NotNull(certificate);
             Assert.Equal(thumbprint, certificate.Thumbprint);
         });
+    }
+
+    /// <summary>
+    ///     Tests that <seealso cref="ICertificateService.AddCertificate(string, X509Certificate2)"/>
+    ///     adds a self-signed test certificate to the certificate store, and that subsequently
+    ///     <seealso cref="ICertificateService.RemoveCertificate(string, string)"/> removes the
+    ///     self-signed test certificate from the certificate store.
+    /// </summary>
+    [Fact]
+    public void TestAddRemoveCertificate()
+    {
+        // Arrange
+        string storeService = this._sut.ConfiguredStoreService;
+        var certificate1 = CreateSelfSignedCertificate("O=AdvancedSystems");
+        string thumbprint = certificate1.Thumbprint;
+
+        // Act
+        var certificateService = this._sut.Host?.Services.GetService<ICertificateService>();
+        bool isAdded = certificateService?.AddCertificate(storeService, certificate1) ?? false;
+        X509Certificate2? certificate2 = certificateService?.GetCertificate(storeService, thumbprint, validOnly: false);
+        bool isRemoved = certificateService?.RemoveCertificate(storeService, thumbprint) ?? false;
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.NotNull(certificateService);
+            Assert.True(isAdded);
+            Assert.NotNull(certificate2);
+            Assert.True(isRemoved);
+        });
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private static X509Certificate2 CreateSelfSignedCertificate(string subject)
+    {
+        using var csdsa = ECDsa.Create();
+        var request = new CertificateRequest(subject, csdsa, HashAlgorithmName.SHA256);
+        var validFrom = DateTimeOffset.UtcNow;
+        X509Certificate2 certificate = request.CreateSelfSigned(validFrom, validFrom.AddHours(1));
+        return certificate;
     }
 
     #endregion
