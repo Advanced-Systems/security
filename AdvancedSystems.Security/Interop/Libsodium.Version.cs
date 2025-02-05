87af8c170ed77d92d20aeca8a3038e1eefe1e2ca
StefanGreve <greve.stefan@outlook.jp>
2025-02-05 22:41:39 +0100

    Restructure libsodium code

--- AdvancedSystems.Security/Interop/Libsodium.Version.cs
+++ AdvancedSystems.Security/Interop/Libsodium.Version.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace AdvancedSystems.Security.Interop;
 
-internal partial class Libsodium
+internal static partial class Libsodium
 {
-    #region version.c
-
     [LibraryImport(NativeLibrary.LIBSODIUM, EntryPoint = "sodium_library_version_major")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     internal static partial int SodiumLibraryVersionMajor();
 
     [LibraryImport(NativeLibrary.LIBSODIUM, EntryPoint = "sodium_library_version_minor")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     internal static partial int SodiumLibraryVersionMinor();
 
     [LibraryImport(NativeLibrary.LIBSODIUM, EntryPoint = "sodium_version_string")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
     internal static partial IntPtr SodiumVersionString();
-
-    #endregion
 }
--- docs/docs/providers.md
+++ docs/docs/providers.md
@@ -8,10 +8,6 @@ TODO
 
 ## Hash
 
-## Generic Hash Algorithms
-
-## PBKDF2 Derived Keys
-
 TODO
 
 ## RSACryptoProvider
