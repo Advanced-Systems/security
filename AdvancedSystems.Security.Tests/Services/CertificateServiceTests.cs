c8564a4a2fb582b24d70d77d72e022cb0f54f1a4
StefanGreve <greve.stefan@outlook.jp>
2025-02-13 23:37:53 +0100

    Improve documentation for RSA options

--- AdvancedSystems.Security.Tests/Services/CertificateServiceTests.cs
+++ AdvancedSystems.Security.Tests/Services/CertificateServiceTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 using AdvancedSystems.Security.Abstractions;
--- AdvancedSystems.Security/Options/RSACryptoOptions.cs
+++ AdvancedSystems.Security/Options/RSACryptoOptions.cs
@@ -5,19 +5,39 @@ using AdvancedSystems.Security.Abstractions;
 
 namespace AdvancedSystems.Security.Options;
 
+/// <summary>
+///     Configures options for the <seealso cref="IRSACryptoService"/>.
+/// </summary>
 public sealed record RSACryptoOptions
 {
+    /// <summary>
+    ///     <inheritdoc cref="Abstractions.HashFunction"/>
+    /// </summary>
     [Required]
+    [EnumDataType(typeof(HashFunction))]
     public required HashFunction HashFunction { get; set; }
 
+    /// <summary>
+    ///     <inheritdoc cref="RSAEncryptionPadding"/>
+    /// </summary>
     [Required]
     public required RSAEncryptionPadding EncryptionPadding { get; set; }
 
+    /// <summary>
+    ///     <inheritdoc cref="RSASignaturePadding"/>
+    /// </summary>
     [Required]
     public required RSASignaturePadding SignaturePadding { get; set; }
 
+    /// <summary>
+    ///     The string representing the thumbprint of the encryption certificate to retrieve.
+    /// </summary>
     [Required]
     public required string Thumbprint { get; set; }
 
+    /// <summary>
+    ///     Set this value to <see langword="true"/> to allow only valid certificates to be
+    ///     used for the encryption and decryption; otherwise, <see langword="false"/>.
+    /// </summary>
     public bool RequireValidCertificate { get; set; } = true;
 }
